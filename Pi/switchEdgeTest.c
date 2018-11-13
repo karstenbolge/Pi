6f2b974bd966738c50babe3a17a0dcaab34a6588
KarstenUBolge <karsten.bolge@laerdal.com>
2018-11-13 15:40:44 +0100

    Added SwitchEdgeTest

---
+++ Pi/switchEdgeTest.c
@@ -0,0 +1,17 @@
+#include "switchEdgeTest.h"
+
+void switchEdgeTestOpen() {
+  printf("switchEdgeTestOpen");
+}
+
+void switchEdgeTestUp() {
+  printf("switchEdgeTestUp");
+}
+
+void switchEdgeTestDown() {
+  printf("switchEdgeTestDown");
+}
+
+void switchEdgeTestEnter() {
+  printf("switchEdgeTestEnter");
+}
---
+++ Pi/switchEdgeTest.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void switchEdgeTestOpen();
+void switchEdgeTestUp();
+void switchEdgeTestDown();
+void switchEdgeTestEnter();
