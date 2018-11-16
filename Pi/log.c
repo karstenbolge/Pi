16b361a9827f0383ea215dae2ec08cb9554a597f
KarstenUBolge <karsten.bolge@laerdal.com>
2018-11-16 14:29:58 +0100

    Improved logging

--- Pi/log.c
+++ Pi/log.c
@@ -1,4 +1,29 @@
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 #include "log.h"
 
+void initLog() {
+  pLog = fopen("log.txt", "a");
+  if (pLog == NULL) {
+	exit(1);
+  }
+}
+
+void closeLog() {
+  fclose(pLog);
+}
+
 void writeLog(uint8_t id) {
+  fprintf(pLog, "%s\n", id);
+}
+
+void writeLogString(char *pString) {
+  time_t rawtime;
+  struct tm * timeinfo;
+
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+
+  fprintf(pLog, "[%d-%02d-%02d %02d:%02d:%02d] %s\n", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, pString);
 }
--- Pi/log.h
+++ Pi/log.h
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
 #define SHOULD_NEVER_HAPPENS 1
 
+FILE *pLog;
+
+void initLog();
+void closeLog();
 void writeLog(uint8_t id);
+void writeLogString(char *pString);
