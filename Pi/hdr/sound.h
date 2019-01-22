54f6876d94dab166c215404cf9c6c7c3a417c7e2
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-01-22 13:49:07 +0100

    Added sound support

--- Pi/hdr/sound.h
+++ Pi/hdr/sound.h
@@ -1,7 +1,15 @@
 #ifndef _SOUND_H_
 #define _SOUND_H_
 
+#define WRONG_SOUND "wrong.wav"
+#define OK_SOUND "ok.wav"
+#define ENTER_SOUND "enter.wav"
+#define BACK_SOUND "back.wav"
+
 void playSoundWrong();
+void playSoundOk();
+void playSoundEnter();
+void playSoundBack();
 void volumnUp();
 void volumnDown();
 
