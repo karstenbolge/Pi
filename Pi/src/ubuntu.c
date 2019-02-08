c78e07bb09d45424c931063813acaf5463d9754b
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-02-08 09:15:17 +0100

    Added file

---
+++ Pi/src/ubuntu.c
@@ -0,0 +1,19 @@
+#include "../hdr/platform.h"
+#include <stdio.h>
+
+void setupWiring()
+{
+}
+
+void updateColumn(uint8_t column)
+{
+}
+
+void updateShiftIn()
+{
+}
+
+void platform()
+{
+  printf("Running on Ubuntu\n");
+}
