22aa8a68a94722ff8b9fea32f632f091386d0c92
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-02-08 09:14:55 +0100

    Added platform

---
+++ Pi/hdr/platform.h
@@ -0,0 +1,21 @@
+#ifndef _PLATFORM_H_
+#define _PLATFORM_H_
+
+#include <stdint.h>
+
+#define COLUMN_DATA_PIN 7
+#define COLUMN_CLOCK_PIN 8
+#define COLUMN_LATCH_PIN 9
+
+#define INPUT_SWITCH_DATA_PIN 1   //green
+#define INPUT_SWITCH_CLOCK_PIN 15 //yellow
+#define INPUT_SWITCH_LATCH_PIN 16 //White
+#define INPUT_SWITCH_ENABLE_PIN 4 //White
+
+void setupWiring();
+void updateColumn(uint8_t column);
+void updateShiftIn();
+
+void platform();
+
+#endif
