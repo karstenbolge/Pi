c7ceebb5b60171725060febddeb8602d53469d31
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-02-08 14:52:52 +0100

    Added stuff

---
+++ Pi/hdr/window.h
@@ -0,0 +1,21 @@
+#ifndef _WINDOW_H_
+#define _WINDOW_H_
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../hdr/platform.h"
+
+Display *display;
+
+void createWindow();
+void closeWindow();
+void redrawWindow();
+
+void refreshDmd();
+
+#endif
