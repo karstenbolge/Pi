f083da26f455e5022fce924e1470fded0148305e
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-02-14 13:51:01 +0100

    Using alsa sounds

--- Pi/hdr/sound.h
+++ Pi/hdr/sound.h
@@ -1,11 +1,19 @@
 #ifndef _SOUND_H_
 #define _SOUND_H_
 
-#define WRONG_SOUND "wrong.wav"
-#define OK_SOUND "ok.wav"
-#define ENTER_SOUND "enter.wav"
-#define BACK_SOUND "back.wav"
+// https://freewavesamples.com/
+#define WRONG_SOUND 0
+#define OK_SOUND 1
+#define ENTER_SOUND 2
+#define BACK_SOUND 3
 
+#define NUMBER_OF_SOUNDS 4
+
+#define MAX_VOLUMN 20
+
+void *initAlsa(void *pSoundNumber);
+void initSounds();
+void initSounds();
 void playSoundWrong();
 void playSoundOk();
 void playSoundEnter();
