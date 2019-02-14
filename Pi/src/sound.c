a40788d39e5882874028ecca680acd44bf2ac1d1
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-02-14 13:51:06 +0100

    Using alsa sounds

--- Pi/src/sound.c
+++ Pi/src/sound.c
@@ -1,3 +1,6 @@
+//https://cboard.cprogramming.com/linux-programming/139389-simple-c-library-playing-wav-files.html
+//https://gist.github.com/ghedo/963382
+// sudo apt-get install libasound2 libasound2-dev
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -5,13 +8,168 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <alsa/asoundlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/stat.h>
 
 #include "../hdr/sound.h"
 
-uint8_t volumne = 8;
+long volumne = 8;
+int soundNo;
+
+pthread_t soundThread;
+
+#define PCM_DEVICE "default"
+
+typedef struct soundItem
+{
+  char *data;
+  long length;
+} soundItem_t;
+
+soundItem_t sounds[NUMBER_OF_SOUNDS];
+
+char *loadWavFile(char *file, long *fileLength)
+{
+  size_t rv;
+  struct stat info;
+  char *data;
+  FILE *pFile = fopen(file, "rb");
+
+  if (pFile == NULL)
+  {
+    fprintf(stderr, "loadWavFile() -- Couldn't open %s: ", file);
+    return NULL;
+  }
+
+  stat(file, &info);
+  data = malloc(info.st_size);
+  if ((rv = fread(data, 1, info.st_size, pFile)) != info.st_size)
+  {
+    fprintf(stderr, "loadWavFile -- Read short on %s: %d/%d\n", file, (int)rv, (int)info.st_size);
+  }
+  fclose(pFile);
+  *fileLength = info.st_size;
+  return data;
+}
+
+void initSounds()
+{
+  sounds[WRONG_SOUND].data = loadWavFile("./sounds/boing_x.wav", &sounds[WRONG_SOUND].length);
+  sounds[OK_SOUND].data = loadWavFile("./sounds/cash_register_x.wav", &sounds[OK_SOUND].length);
+  sounds[ENTER_SOUND].data = loadWavFile("./sounds/whah_whah.wav", &sounds[ENTER_SOUND].length);
+  sounds[BACK_SOUND].data = loadWavFile("./sounds/Roland-JD-990-Windchimes.wav", &sounds[BACK_SOUND].length);
+}
+
+void *initAlsa(void *pSoundNumber)
+{
+  unsigned int pcm, tmp, dir;
+  int rate, channels;
+  snd_pcm_t *pcm_handle;
+  snd_pcm_hw_params_t *params;
+  snd_pcm_uframes_t frames;
+
+  rate = 44100;
+  channels = 2;
+
+  /* Open the PCM device in playback mode */
+  if (pcm = snd_pcm_open(&pcm_handle, PCM_DEVICE,
+                         SND_PCM_STREAM_PLAYBACK, 0) < 0)
+    printf("ERROR: Can't open \"%s\" PCM device. %s\n",
+           PCM_DEVICE, snd_strerror(pcm));
+
+  /* Allocate parameters object and fill it with default values*/
+  snd_pcm_hw_params_alloca(&params);
+
+  snd_pcm_hw_params_any(pcm_handle, params);
+
+  /* Set parameters */
+  if (pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
+                                         SND_PCM_ACCESS_RW_INTERLEAVED) < 0)
+    printf("ERROR: Can't set interleaved mode. %s\n", snd_strerror(pcm));
+
+  if (pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
+                                         SND_PCM_FORMAT_S16_LE) < 0)
+    printf("ERROR: Can't set format. %s\n", snd_strerror(pcm));
+
+  if (pcm = snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0)
+    printf("ERROR: Can't set channels number. %s\n", snd_strerror(pcm));
+
+  if (pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0) < 0)
+    printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
+
+  /* Write parameters */
+  if (pcm = snd_pcm_hw_params(pcm_handle, params) < 0)
+    printf("ERROR: Can't set harware parameters. %s\n", snd_strerror(pcm));
+
+  /* Resume information */
+  printf("PCM name: '%s'\n", snd_pcm_name(pcm_handle));
+
+  printf("PCM state: %s\n", snd_pcm_state_name(snd_pcm_state(pcm_handle)));
+
+  snd_pcm_hw_params_get_channels(params, &tmp);
+  printf("channels: %i ", tmp);
+
+  if (tmp == 1)
+    printf("(mono)\n");
+  else if (tmp == 2)
+    printf("(stereo)\n");
+
+  snd_pcm_hw_params_get_rate(params, &tmp, 0);
+  printf("rate: %d bps\n", tmp);
+
+  //printf("seconds: %d\n", seconds);
+
+  // Allocate buffer to hold single period
+  snd_pcm_hw_params_get_period_size(params, &frames, 0);
+
+  printf("frames: %ld\n", frames);
+
+  /*
+  snd_pcm_hw_params_get_period_time(params, &tmp, NULL);*/
+
+  int rc;
+  int size = frames * 4;
+  int *pNumber = (int *)pSoundNumber;
+  ///int rem;
+  char *dp = sounds[*pNumber].data + 44; // wave headeer is first 44 bytes
+  for (int i = 0; i < sounds[*pNumber].length - 44; i += size)
+  {
+    //printf("playing %d\n", i);
+    // rem = (soundsLen[3] - i) / 2;
+    //if (rem > 128) rem = 128;
+    rc = snd_pcm_writei(pcm_handle, dp, frames);
+    if (rc == -EPIPE)
+    {
+      /* EPIPE means underrun */
+      printf("underrun occurred\n");
+      snd_pcm_prepare(pcm_handle);
+    }
+    else if (rc < 0)
+    {
+      printf("error from writei: %d\n", rc);
+    }
+    else if (rc != (int)frames)
+    {
+      printf("short write, write %d frames\n", rc);
+    }
+
+    dp += size;
+  }
+
+  printf("done playing\n");
+  snd_pcm_drain(pcm_handle);
+  printf("drained\n");
+  snd_pcm_close(pcm_handle);
+  printf("closed\n");
+
+  return 0;
+}
 
 int playFile(char *p)
 {
+  /* old
   int status;
   pid_t pid;
 
@@ -19,53 +177,99 @@ int playFile(char *p)
   if (pid == 0)
   {
     printf("1.\n");
-    /* This is the child process.  Execute the shell command. */
+    // This is the child process.  Execute the shell command. 
     execl("/usr/bin/omxplayer", " ", p, NULL);
     printf("2.\n");
     _exit(EXIT_FAILURE);
   }
   else if (pid < 0)
-    /* The fork failed.  Report failure.  */
+    // The fork failed.  Report failure.  
     status = -1;
   else
-      /* This is the parent process.  Wait for the child to complete.  */
+      // This is the parent process.  Wait for the child to complete.  
       if (waitpid(pid, &status, 0) != pid)
     status = -1;
-  return status;
+  return status;*/
 }
 
 void playSoundWrong()
 {
-  playFile(WRONG_SOUND);
+  soundNo = WRONG_SOUND;
+  if (pthread_create(&soundThread, NULL, initAlsa, &soundNo))
+  {
+    fprintf(stderr, "Error creating thread\n");
+  }
 }
 
 void playSoundOk()
 {
-  playFile(OK_SOUND);
+  soundNo = OK_SOUND;
+  if (pthread_create(&soundThread, NULL, initAlsa, &soundNo))
+  {
+    fprintf(stderr, "Error creating thread\n");
+  }
 }
 
 void playSoundEnter()
 {
-  playFile(ENTER_SOUND);
+  soundNo = ENTER_SOUND;
+  if (pthread_create(&soundThread, NULL, initAlsa, &soundNo))
+  {
+    fprintf(stderr, "Error creating thread\n");
+  }
 }
 
 void playSoundBack()
 {
-  playFile(BACK_SOUND);
+  // https://timmurphy.org/2010/05/04/pthreads-in-c-a-minimal-working-example/
+  soundNo = BACK_SOUND;
+  if (pthread_create(&soundThread, NULL, initAlsa, &soundNo))
+  {
+    fprintf(stderr, "Error creating thread\n");
+  }
+}
+
+void setVolume()
+{
+  printf("volumn seet to %ld\n", volumne);
+  long min, max;
+  snd_mixer_t *handle;
+  snd_mixer_selem_id_t *pSelemId;
+  const char *pCard = "default";
+  const char *pSelemName = "Master";
+
+  snd_mixer_open(&handle, 0);
+  snd_mixer_attach(handle, pCard);
+  snd_mixer_selem_register(handle, NULL, NULL);
+  snd_mixer_load(handle);
+
+  snd_mixer_selem_id_alloca(&pSelemId);
+  snd_mixer_selem_id_set_index(pSelemId, 0);
+  snd_mixer_selem_id_set_name(pSelemId, pSelemName);
+  snd_mixer_elem_t *pElement = snd_mixer_find_selem(handle, pSelemId);
+
+  snd_mixer_selem_get_playback_volume_range(pElement, &min, &max);
+  snd_mixer_selem_set_playback_volume_all(pElement, volumne * max / MAX_VOLUMN);
+
+  snd_mixer_close(handle);
 }
 
 void volumnUp()
 {
-  if (volumne < 20)
+  if (volumne < MAX_VOLUMN)
   {
     volumne++;
   }
+
+  setVolume();
 }
 
 void volumnDown()
 {
-  if (volumne > 2)
+  if (volumne > 1)
   {
     volumne--;
   }
+
+  setVolume();
 }
