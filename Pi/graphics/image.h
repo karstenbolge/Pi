c5460c739f746afdafd3977ec327735110b1b69c
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-02-26 10:02:21 +0100

    Added file

---
+++ Pi/graphics/image.h
@@ -0,0 +1,11 @@
+#ifndef _IMAGE_H_
+#define _IMAGE_H_
+char *getImage00161();
+char *getImage00158();
+char *getImage00155();
+char *getImage00152();
+char *getImage00035();
+char *getImage00150();
+char *getImage00163();
+#endif
+
