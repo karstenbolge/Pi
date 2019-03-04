bdb29d544961e9d8656eeddd7d05954e6c6d8a5e
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-03-04 09:51:30 +0100

    Updated

--- Pi/graphics/image.h
+++ Pi/graphics/image.h
@@ -1,21 +1,21 @@
 #ifndef _IMAGE_H_
 #define _IMAGE_H_
 char *getImage00137();
-char *getImage00155();
-char *getImage00130();
-char *getImage00035();
-char *getImage00127();
-char *getImage00158();
-char *getImage00133();
-char *getImage00123();
-char *getImage00135();
-char *getImage00128();
-char *getImage00150();
-char *getImage00163();
 char *getImage00161();
-char *getImage00139();
+char *getImage00158();
+char *getImage00130();
+char *getImage00155();
+char *getImage00128();
+char *getImage00133();
 char *getImage00152();
-char *getImage00124();
+char *getImage00127();
 char *getImage00125();
+char *getImage00035();
+char *getImage00124();
+char *getImage00135();
+char *getImage00150();
+char *getImage00123();
+char *getImage00139();
+char *getImage00163();
 #endif
 
