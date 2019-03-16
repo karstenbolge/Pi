b4a76e015646480889804dbc42de7a199971f1cd
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-03-16 12:07:57 +0100

    More image handling

---
+++ Pi/graphics/Explosions/resize.sh
@@ -0,0 +1,113 @@
+convert -resize 800X480  vlcsnap-02279\[cropped\].png explosion-1-02279.png
+
+convert -resize 800X480  vlcsnap-02280\[cropped\].png explosion-1-02280.png
+
+convert -resize 800X480  vlcsnap-02281\[cropped\].png explosion-1-02281.png
+
+convert -resize 800X480  vlcsnap-02282\[cropped\].png explosion-1-02282.png
+
+convert -resize 800X480  vlcsnap-02283\[cropped\].png explosion-1-02283.png
+
+convert -resize 800X480  vlcsnap-02284\[cropped\].png explosion-1-02284.png
+
+convert -resize 800X480  vlcsnap-02285\[cropped\].png explosion-1-02285.png
+
+convert -resize 800X480  vlcsnap-02286\[cropped\].png explosion-1-02286.png
+
+convert -resize 800X480  vlcsnap-02287\[cropped\].png explosion-1-02287.png
+
+convert -resize 800X480  vlcsnap-02288\[cropped\].png explosion-1-02288.png
+
+convert -resize 800X480  vlcsnap-02289\[cropped\].png explosion-1-02289.png
+
+convert -resize 800X480  vlcsnap-02290\[cropped\].png explosion-1-02290.png
+
+convert -resize 800X480  vlcsnap-02291\[cropped\].png explosion-1-02291.png
+
+convert -resize 800X480  vlcsnap-02292\[cropped\].png explosion-1-02292.png
+
+convert -resize 800X480  vlcsnap-02293\[cropped\].png explosion-1-02293.png
+
+convert -resize 800X480  vlcsnap-02294\[cropped\].png explosion-1-02294.png
+
+convert -resize 800X480  vlcsnap-02295\[cropped\].png explosion-1-02295.png
+
+convert -resize 800X480  vlcsnap-02296\[cropped\].png explosion-1-02296.png
+
+convert -resize 800X480  vlcsnap-02297\[cropped\].png explosion-1-02297.png
+
+convert -resize 800X480  vlcsnap-02298\[cropped\].png explosion-1-02298.png
+
+convert -resize 800X480  vlcsnap-02299\[cropped\].png explosion-1-02299.png
+
+convert -resize 800X480  vlcsnap-02300\[cropped\].png explosion-1-02300.png
+
+convert -resize 800X480  vlcsnap-02301\[cropped\].png explosion-1-02301.png
+
+convert -resize 800X480  vlcsnap-02302\[cropped\].png explosion-1-02302.png
+
+convert -resize 800X480  vlcsnap-02303\[cropped\].png explosion-1-02303.png
+
+convert -resize 800X480  vlcsnap-02304\[cropped\].png explosion-1-02304.png
+
+convert -resize 800X480  vlcsnap-02305\[cropped\].png explosion-1-02305.png
+
+convert -resize 800X480  vlcsnap-02306\[cropped\].png explosion-1-02306.png
+
+convert -resize 800X480  vlcsnap-02307\[cropped\].png explosion-1-02307.png
+
+convert -resize 800X480  vlcsnap-02308\[cropped\].png explosion-1-02308.png
+
+convert -resize 800X480  vlcsnap-02309\[cropped\].png explosion-1-02309.png
+
+convert -resize 800X480  vlcsnap-02310\[cropped\].png explosion-1-02310.png
+
+convert -resize 800X480  vlcsnap-02311\[cropped\].png explosion-1-02311.png
+
+convert -resize 800X480  vlcsnap-02312\[cropped\].png explosion-1-02312.png
+
+convert -resize 800X480  vlcsnap-02313\[cropped\].png explosion-1-02313.png
+
+convert -resize 800X480  vlcsnap-02314\[cropped\].png explosion-1-02314.png
+
+convert -resize 800X480  vlcsnap-02315\[cropped\].png explosion-1-02315.png
+
+convert -resize 800X480  vlcsnap-02316\[cropped\].png explosion-1-02316.png
+
+convert -resize 800X480  vlcsnap-02317\[cropped\].png explosion-1-02317.png
+
+convert -resize 800X480  vlcsnap-02318\[cropped\].png explosion-1-02318.png
+
+convert -resize 800X480  vlcsnap-02319\[cropped\].png explosion-1-02319.png
+
+convert -resize 800X480  vlcsnap-02320\[cropped\].png explosion-1-02320.png
+
+convert -resize 800X480  vlcsnap-02321\[cropped\].png explosion-1-02321.png
+
+convert -resize 800X480  vlcsnap-02322\[cropped\].png explosion-1-02322.png
+
+convert -resize 800X480  vlcsnap-02323\[cropped\].png explosion-1-02323.png
+
+convert -resize 800X480  vlcsnap-02324\[cropped\].png explosion-1-02324.png
+
+convert -resize 800X480  vlcsnap-02325\[cropped\].png explosion-1-02325.png
+
+convert -resize 800X480  vlcsnap-02326\[cropped\].png explosion-1-02326.png
+
+convert -resize 800X480  vlcsnap-02327\[cropped\].png explosion-1-02327.png
+
+convert -resize 800X480  vlcsnap-02328\[cropped\].png explosion-1-02328.png
+
+convert -resize 800X480  vlcsnap-02329\[cropped\].png explosion-1-02329.png
+
+convert -resize 800X480  vlcsnap-02330\[cropped\].png explosion-1-02330.png
+
+convert -resize 800X480  vlcsnap-02331\[cropped\].png explosion-1-02331.png
+
+convert -resize 800X480  vlcsnap-02332\[cropped\].png explosion-1-02332.png
+
+convert -resize 800X480  vlcsnap-02333\[cropped\].png explosion-1-02333.png
+
+convert -resize 800X480  vlcsnap-02334\[cropped\].png explosion-1-02334.png
+
+convert -resize 800X480  vlcsnap-02335\[cropped\].png explosion-1-02335.png
