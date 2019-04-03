9ca3d47e28e2b167882500072db4a5cff7e41f40
KarstenBolge <KarstenBolge@Yahoo.dk>
2019-04-03 09:34:05 +0200

    Rewriten test buttons

--- Pi/src/ubuntu.c
+++ Pi/src/ubuntu.c
@@ -7,6 +7,30 @@
 #include <unistd.h>
 #include <sys/select.h>
 
+#define BUTTONS_COUNT 11
+
+typedef struct pointItem
+{
+  int x;
+  int y;
+} pointItem_t;
+
+typedef struct buttonItem
+{
+  char name[32];
+  pointItem_t from;
+  pointItem_t to;
+  unsigned long color;
+  unsigned long frameColor;
+  unsigned long textColor;
+  uint16_t registry;
+  uint8_t column;
+} buttonItem_t;
+
+XFontStruct *fontinfo;
+
+buttonItem_t buttons[BUTTONS_COUNT];
+
 const int button1x = 10;
 const int button1y = 485;
 const int button2x = 60;
@@ -30,11 +54,11 @@ const int buttonCancelY = 485;
 const int buttonBallEndedX = 750;
 const int buttonBallEndedY = 485;
 
-const int buttonBeatX = 250;
-const int buttonBeatY = 485;
+const int buttonBeatX = 10;
+const int buttonBeatY = 535;
 
 const int gridViewX = 320;
-const int gridViewY = 485;
+const int gridViewY = 535;
 
 const int buttonWidth = 40;
 const int buttonHeight = 40;
@@ -42,60 +66,86 @@ int onButton = 0;
 
 int oldCursorX, oldCursorY;
 
+void makeButton(int i, char *pName, int fromX, int fromY, int toX, int toY, unsigned long color, unsigned long frameColor, unsigned long textColor, uint16_t registry, uint8_t column)
+{
+  strcpy(buttons[i].name, pName);
+  buttons[i].from.x = fromX;
+  buttons[i].from.y = fromY;
+  buttons[i].to.x = toX;
+  buttons[i].to.y = toY;
+  buttons[i].color = color;
+  buttons[i].frameColor = frameColor;
+  buttons[i].textColor = textColor;
+  buttons[i].registry = registry;
+  buttons[i].column = column;
+}
+
+void drawButtons()
+{
+  int direction;
+  int ascent;
+  int descent;
+  XCharStruct overall;
+
+  for (int i = 0; i < BUTTONS_COUNT; i++)
+  {
+    XSetForeground(display, gc, buttons[i].frameColor);
+    XFillRectangle(display, win, gc, buttons[i].from.x, buttons[i].from.y, buttons[i].to.x - buttons[i].from.x, buttons[i].to.y - buttons[i].from.y);
+    XSetForeground(display, gc, buttons[i].color);
+    XFillRectangle(display, win, gc, buttons[i].from.x + 2, buttons[i].from.y + 2, buttons[i].to.x - buttons[i].from.x - 4, buttons[i].to.y - buttons[i].from.y - 4);
+
+    //https://www.lemoda.net/c/xlib-text-box/
+    XTextExtents(fontinfo, buttons[i].name, strlen(buttons[i].name), &direction, &ascent, &descent, &overall);
+    XSetForeground(display, gc, buttons[i].textColor);
+    XDrawString(display, win, gc, (buttons[i].to.x + buttons[i].from.x - overall.width) / 2, (buttons[i].to.y + buttons[i].from.y + ascent - descent) / 2, buttons[i].name, strlen(buttons[i].name));
+  }
+}
+
+#define MAX_FONTS 30000
+void showFonts()
+{
+  int nFonts;
+  char **szaFontNames;
+  szaFontNames = XListFonts(display, "*", MAX_FONTS, &nFonts);
+  if (nFonts == MAX_FONTS)
+  {
+    printf("Many fonts on your system.  Not displaying all.");
+  }
+
+  for (int i = 0; i < nFonts; i++)
+  {
+    printf("%s\n", szaFontNames[i]);
+  }
+
+  XFreeFontNames(szaFontNames);
+}
+
 void setupWiring()
 {
   onBeat = &onBeatFunction;
+  //showFonts();
+
+  fontinfo = XLoadQueryFont(display, "7x13bold"); // 6x10 ubuntu font size
+  int rc = XSetFont(display, gc, fontinfo->fid);
 
   // draw debug area
   XSetForeground(display, gc, colorRed.pixel);
   XFillRectangle(display, win, gc, 0, 480, 799, 529);
 
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, button1x, button1y, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorBlack.pixel);
-  XFillRectangle(display, win, gc, button1x + 2, button1y + 2, buttonWidth - 4, buttonHeight - 4);
+  makeButton(0, "Esc", 10, 485, 49, 524, colorBlack.pixel, colorGreen.pixel, colorWhite.pixel, 1 << 2, 2);
+  makeButton(1, "Up", 60, 485, 99, 524, colorGreen.pixel, colorBlack.pixel, colorBlack.pixel, 1 << 12, 2);
+  makeButton(2, "Down", 110, 485, 149, 524, colorWhite.pixel, colorBlack.pixel, colorBlack.pixel, 1 << 15, 2);
+  makeButton(3, "Enter", 160, 485, 199, 524, colorBlue.pixel, colorGreen.pixel, colorWhite.pixel, 1 << 3, 2);
 
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, button2x, button2y, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, button2x + 2, button2y + 2, buttonWidth - 4, buttonHeight - 4);
+  makeButton(4, "Start", 250, 485, 289, 524, colorGreen.pixel, colorBlack.pixel, colorBlack.pixel, 1 << 10, 2);
+  makeButton(5, "Plung", 300, 485, 339, 524, colorBlack.pixel, colorGreen.pixel, colorWhite.pixel, 1 << 13, 2);
+  makeButton(6, "Both", 350, 485, 389, 524, colorWhite.pixel, colorBlack.pixel, colorBlack.pixel, (1 << 5) + (1 << 6), 2);
+  makeButton(7, "Left", 400, 485, 439, 524, colorGreen.pixel, colorBlack.pixel, colorBlack.pixel, 1 << 5, 2);
+  makeButton(8, "Rigth", 450, 485, 489, 524, colorRed.pixel, colorBlack.pixel, colorBlack.pixel, 1 << 6, 2);
+  makeButton(9, "End", 500, 485, 539, 524, colorBlack.pixel, colorGreen.pixel, colorWhite.pixel, 1 << 14, 2);
+  makeButton(10, "PEZ", 500, 485, 539, 524, colorWhite.pixel, colorBlack.pixel, colorRed.pixel, 1 << 11, 2);
 
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, button3x, button3y, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorWhite.pixel);
-  XFillRectangle(display, win, gc, button3x + 2, button3y + 2, buttonWidth - 4, buttonHeight - 4);
-
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, button4x, button4y, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorBlue.pixel);
-  XFillRectangle(display, win, gc, button4x + 2, button4y + 2, buttonWidth - 4, buttonHeight - 4);
-
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, plungerX, plungerY, buttonWidth / 2, buttonHeight);
-  XSetForeground(display, gc, colorBlack.pixel);
-  XFillRectangle(display, win, gc, plungerX + 2, plungerY + 2, buttonWidth / 2 - 4, buttonHeight - 4);
-
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, buttonStartX, buttonStartY, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorBlue.pixel);
-  XFillRectangle(display, win, gc, buttonStartX + 2, buttonStartY + 2, buttonWidth - 4, buttonHeight - 4);
-
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, buttonExtraX, buttonExtraY, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorBlack.pixel);
-  XFillRectangle(display, win, gc, buttonExtraX + 2, buttonExtraY + 2, buttonWidth - 4, buttonHeight - 4);
-
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, buttonCancelX, buttonCancelY, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorBlue.pixel);
-  XFillRectangle(display, win, gc, buttonCancelX + 2, buttonCancelY + 2, buttonWidth / 2 - 3, buttonHeight / 2 - 3);
-  XFillRectangle(display, win, gc, buttonCancelX + 1 + buttonWidth / 2, buttonCancelY + 2, buttonWidth / 2 - 3, buttonHeight / 2 - 3);
-  XFillRectangle(display, win, gc, buttonCancelX + 2, buttonCancelY + 2 + buttonHeight / 2, buttonWidth - 4, buttonHeight / 2 - 3);
-
-  XSetForeground(display, gc, colorGreen.pixel);
-  XFillRectangle(display, win, gc, buttonBallEndedX, buttonBallEndedY, buttonWidth, buttonHeight);
-  XSetForeground(display, gc, colorBlack.pixel);
-  XFillRectangle(display, win, gc, buttonBallEndedX + 2, buttonBallEndedY + 2, buttonWidth - 4, buttonHeight - 4);
+  drawButtons();
 
   XSetForeground(display, gc, colorBlack.pixel);
   // horizontal
@@ -119,11 +169,6 @@ void updateShiftIn()
 {
   newInputRegister = 0;
 
-  if (column != 2)
-  {
-    return;
-  }
-
   int cursorX, cursorY, rootWindowX, rootWindowY = 0;
   unsigned int mask = 0;
   Window childWindow, rootWindow;
@@ -137,120 +182,21 @@ void updateShiftIn()
   XTranslateCoordinates(display, win, DefaultRootWindow(display), 0, 0, &x, &y, &child);
   XGetWindowAttributes(display, win, &xwa);
 
-  if (oldCursorX != cursorX || oldCursorY != cursorY)
+  //if (oldCursorX != cursorX || oldCursorY != cursorY)
   {
-    // check button 1
-    if (cursorX >= x + button1x && cursorX <= x + button1x + buttonWidth &&
-        cursorY >= y + button1y && cursorY <= y + button1y + buttonHeight)
+    for (int i = 0; i < BUTTONS_COUNT; i++)
     {
-      onButton = 1;
+      if (buttons[i].column == column)
+      {
+        if (cursorX >= x + buttons[i].from.x && cursorX <= x + buttons[i].to.x &&
+            cursorY >= y + buttons[i].from.y && cursorY <= y + buttons[i].to.y)
+        {
+          newInputRegister = buttons[i].registry;
+        }
+      }
     }
-    else if (cursorX >= x + button2x && cursorX <= x + button2x + buttonWidth &&
-             cursorY >= y + button2y && cursorY <= y + button2y + buttonHeight)
-    {
-      onButton = 2;
-    }
-    else if (cursorX >= x + button3x && cursorX <= x + button3x + buttonWidth &&
-             cursorY >= y + button3y && cursorY <= y + button3y + buttonHeight)
-    {
-      onButton = 3;
-    }
-    else if (cursorX >= x + button4x && cursorX <= x + button4x + buttonWidth &&
-             cursorY >= y + button4y && cursorY <= y + button4y + buttonHeight)
-    {
-      onButton = 4;
-    }
-    else if (cursorX >= x + buttonStartX && cursorX <= x + buttonStartX + buttonWidth &&
-             cursorY >= y + buttonStartY && cursorY <= y + buttonStartY + buttonHeight)
-    {
-      onButton = 5;
-    }
-    else if (cursorX >= x + buttonExtraX && cursorX <= x + buttonExtraX + buttonWidth &&
-             cursorY >= y + buttonExtraY && cursorY <= y + buttonExtraY + buttonHeight)
-    {
-      onButton = 6;
-    }
-    else if (cursorX >= x + buttonCancelX && cursorX <= x + buttonCancelX + buttonWidth &&
-             cursorY >= y + buttonCancelY && cursorY <= y + buttonCancelY + buttonHeight)
-    {
-      if (cursorX >= x + buttonCancelX && cursorX <= x + buttonCancelX + buttonWidth / 2 &&
-          cursorY >= y + buttonCancelY && cursorY <= y + buttonCancelY + buttonHeight / 2)
-        onButton = 10;
-      else if (cursorX >= x + buttonCancelX + buttonWidth / 2 && cursorX <= x + buttonCancelX + buttonWidth &&
-               cursorY >= y + buttonCancelY && cursorY <= y + buttonCancelY + buttonHeight / 2)
-        onButton = 11;
-      else
-        onButton = 7;
-    }
-    else if (cursorX >= x + buttonBallEndedX && cursorX <= x + buttonBallEndedX + buttonWidth &&
-             cursorY >= y + buttonBallEndedY && cursorY <= y + buttonBallEndedY + buttonHeight)
-    {
-      onButton = 8;
-    }
-    else if (cursorX >= x + plungerX && cursorX <= x + plungerX + buttonWidth &&
-             cursorY >= y + plungerY && cursorY <= y + plungerY + buttonHeight)
-    {
-      onButton = 9;
-    }
-    else
-    {
-      onButton = 0;
-    }
-
     oldCursorX = cursorX;
     oldCursorY = cursorY;
-  }
-
-  switch (onButton)
-  {
-  case 1:
-    // set escape pressed
-    newInputRegister = 1 << 2;
-    break;
-  case 2:
-    // set up pressed
-    newInputRegister = 1 << 12;
-    break;
-  case 3:
-    // set down pressed
-    newInputRegister = 1 << 15;
-    break;
-  case 4:
-    // set enter pressed
-    newInputRegister = 1 << 3;
-    break;
-  case 5:
-    // set start pressed
-    newInputRegister = 1 << 10;
-    break;
-  case 6:
-    // set extraball pressed
-    newInputRegister = 1 << 11;
-    break;
-  case 7:
-    // set left, rigth flipper pressed
-    newInputRegister = (1 << 5) + (1 << 6);
-    break;
-  case 8:
-    // set ball ended
-    newInputRegister = 1 << 14;
-    break;
-  case 9:
-    // set plunger
-    newInputRegister = 1 << 13;
-    break;
-  case 10:
-    // set left flipper pressed
-    newInputRegister = 1 << 5;
-    break;
-  case 11:
-    // set right flipper pressed
-    newInputRegister = 1 << 6;
-    break;
-  default:
-    newInputRegister = 0;
-
-    break;
   }
 }
 
@@ -266,7 +212,7 @@ int getWindowWitdh()
 
 int getWindowHeight()
 {
-  return 530;
+  return 580;
 }
 
 void onBeatFunction(int beat)
